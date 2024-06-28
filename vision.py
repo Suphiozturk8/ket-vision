31fc48a4d8cc6a1f358b9841bdd9820873624a90
ibrahim yöndemli <ket0x4@pm.me>
2024-06-28 19:04:16 +0300

    Fix /vision, refactor, add version and /ping

--- vision.py
+++ vision.py
@@ -2,9 +2,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, logging as hf_logg
 from PIL import Image, ImageOps
 from pyrogram import Client, filters, enums, idle
 import asyncio
-import os
+import os, time
 import logging
-import shutil
 from concurrent.futures import ThreadPoolExecutor
 from dotenv import load_dotenv
 
@@ -15,6 +14,20 @@ load_dotenv()
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
+version = os.getenv("VERSION")
+
+# Configure Telegram bot
+bot = Client(
+    name="ket.vision",
+    api_id=api_id,
+    api_hash=api_hash,
+    bot_token=bot_token,
+    parse_mode=enums.ParseMode.MARKDOWN,
+    skip_updates=True,
+)
+
+# Start message
+
 
 # Logging configuration
 logging.basicConfig(level=logging.INFO)
@@ -25,7 +38,7 @@ description_prompt = "Describe this image."
 question_prompt = "What is happening in this image?"
 
 # Some switches
-autoVision = None
+autoVision = False
 
 # Thread pool executor
 executor = ThreadPoolExecutor()
@@ -41,6 +54,12 @@ tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
 # Function to resize image to 512x512 pixels
 def resize_image(image):
     return ImageOps.fit(image, (512, 512), Image.LANCZOS)
+
+
+# Function to split message length
+def split_message(text, max_length=4096):
+    return [text[i : i + max_length] for i in range(0, len(text), max_length)]
+
 
 # image recognition function
 async def vision(image_path):
@@ -59,7 +78,7 @@ async def vision(image_path):
             result = model.answer_question(enc_image, description_prompt, tokenizer)
             return result
 
-        loop = asyncio.get_event_loop()
+        loop = asyncio.get_running_loop()
         result = await loop.run_in_executor(executor, process_image)
         return result
     except Exception as e:
@@ -67,57 +86,10 @@ async def vision(image_path):
         raise
 
 
-# Configure Telegram bot
-bot = Client(
-    name="ket.vision",
-    api_id=api_id,
-    api_hash=api_hash,
-    bot_token=bot_token,
-    parse_mode=enums.ParseMode.MARKDOWN,
-    skip_updates=True,
-)
-
-
-# Start command
-@bot.on_message(filters.command(["start", "help"]))
-async def start_command(bot, message):
-    user = message.from_user.mention
-    await message.reply_text(
-        "`Ket.vision`\nVersion:`0.3`\n\n"
-        f"Hi {user}. I'm a Telegram bot that uses the multimodels to describe images.\n\n"
-        "**Avaible commands:**\n"
-        "`/vision` - Process an image and describe it.\n"
-        "`/autovision` - Toggle AutoVision mode.\n\n"
-        "**Status:**\n"
-        f"AutoVision mode: `{'enabled' if autoVision else 'disabled'}`\n\n"
-        f"**Model:** `{model_id}`\n"
-    )
-
-
-# Function to split message length
-def split_message(text, max_length=4096):
-    return [text[i : i + max_length] for i in range(0, len(text), max_length)]
-
-# /autovision command
-@bot.on_message(filters.command(["autovision"]))
-async def autovision_command(bot, message):
-    global autovision_enabled
-    if len(message.command) > 1 and message.command[1].lower() in ["on", "off"]:
-        if message.command[1].lower() == "on":
-            autovision_enabled = True
-            await message.reply_text("Automatic description feature is enabled. You will now receive descriptions automatically.")
-        elif message.command[1].lower() == "off":
-            autovision_enabled = False
-            await message.reply_text("Automatic description feature is disabled. You can use /vision command to get descriptions manually.")
-    else:
-        await message.reply_text("Usage: `/autovision` [on/off]")
-
-
 # Image processing command
 @bot.on_message(filters.photo)
-#@bot.on_message(filters.command(["vision"]))
 async def process_image(bot, message):
-    if autovision_enabled:
+    if autoVision:
         download_folder = os.path.join(os.getcwd(), "downloads")
         os.makedirs(download_folder, exist_ok=True)
         image_path = os.path.join(download_folder, "image.jpg")
@@ -145,6 +117,80 @@ async def process_image(bot, message):
                 os.remove(file_path)
                 logger.info(f"Deleted file: {file_path}")
 
+
+# Start command
+@bot.on_message(filters.command(["start", "help"]))
+async def start_command(bot, message):
+    user = message.from_user.mention
+    await message.reply_text(
+        "`Ket.vision`\nVersion:`0.3`\n\n"
+        f"Hi {user}. I'm a Telegram bot that uses the multimodels to describe images.\n\n"
+        "**Avaible commands:**\n"
+        "`/vision` - Process an image and describe it.\n"
+        "`/autovision` - Toggle AutoVision mode.\n\n"
+        "**Status:**\n"
+        f"AutoVision mode: `{'enabled' if autoVision else 'disabled'}`\n\n"
+        f"**Model:** `{model_id}`\n"
+    )
+
+# /autovision command
+@bot.on_message(filters.command(["autovision"]))
+async def autovision_command(bot, message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    global autoVision
+    if len(message.command) > 1 and message.command[1].lower() in ["on", "off"]:
+        if message.command[1].lower() == "on":
+            autoVision = True
+            await message.reply_text(
+                "Autovision enabled for this chat. I will automatically describe images."
+            )
+        elif message.command[1].lower() == "off":
+            autoVision = False
+            await message.reply_text(
+                "Autovision disabled for this chat. I will not automatically describe images."
+            )
+    else:
+        await message.reply_text("Usage: `/autovision` [on/off]")
+
+# Ping command
+@bot.on_message(filters.command(["ping"]))
+async def ping_command(bot, message):
+    start_time = time.time()  # Start time before sending reply
+    reply = await message.reply_text("Calculating latency...")
+    end_time = time.time()  # End time after sending reply
+    
+    # Calculate latency in milliseconds
+    latency = (end_time - start_time) * 1000
+    
+    # Update the message with latency
+    await reply.edit_text(f"**Latency:** `{latency:.2f}`ms")
+
+# /vision command (get replied image and describe it)
+@bot.on_message(filters.command(["vision"]))
+async def vision_command(bot, message):
+    if not message.reply_to_message:
+        await message.reply_text("Reply to an image to describe it.")
+        return
+
+    if message.reply_to_message.photo:
+        await message.reply_text("Processing image...")
+        try:
+            image_path = await message.reply_to_message.download()
+            result = await vision(image_path)
+            for msg in split_message(result):
+                await message.reply_text(msg)
+        except FileNotFoundError as e:
+            await message.reply_text(f"Error: {e}")
+        except Exception as e:
+            await message.reply_text(f"An unexpected error occurred: {e}")
+        finally:
+            # Delete the file
+            if os.path.exists(image_path):
+                os.remove(image_path)
+                logger.info(f"Deleted file: {image_path}")
+    else:
+        await message.reply_text("Reply to an image to describe it.")
 
 # Main function
 async def main():
